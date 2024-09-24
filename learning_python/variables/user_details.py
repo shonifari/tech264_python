61d2cf8d4aa248d36c952bbc50febe66b2d6107c
Karis Omotosho <karis.omotosho@gmail.com>
2024-09-24 16:08:33 +0100

    Added height to user_details

--- learning_python/variables/user_details.py
+++ learning_python/variables/user_details.py
@@ -12,3 +12,23 @@ dob = input("What's your date of birth?: ")
 
 print(f"Hi {name}")
 print(f"You were born on {dob}, therefore your age is {age}")
+
+
+# Use your code from the "Task: Get name, age and DOB details from a user".
+# Mix the name, age and DOB into one list user_details_list
+# Print the user's name, age and DOB from the list
+user_details_list = [name, age , dob]
+print(user_details_list[0])
+print(user_details_list[1])
+print(user_details_list[2])
+
+# Check the age is saved as an integer in the list. If it's not, work out how to convert it to an integer and add the age integer to the list.
+if type(user_details_list[1]) != int:
+    user_details_list[1] = int(user_details_list[1])
+
+# Ask the user for their height in cm and save it to the variable height
+height = input("What's your height in cm?: ")
+
+# Save height as a float in the list, and print the height from the list.
+user_details_list.append(float(height))
+
