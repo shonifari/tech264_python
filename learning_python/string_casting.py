a0709dd1270cbd7cd11219527fd659503d3c8483
Karis Omotosho <karis.omotosho@gmail.com>
2024-09-24 15:06:19 +0100

    Added string_casting

---
+++ learning_python/string_casting.py
@@ -0,0 +1,14 @@
+# String casting 
+int_string = "6"
+
+# convert int_string to an integer
+n = int(int_string)
+
+# after converting, print its data type to the screen
+print(type(n))
+
+# convert int_string to float
+n = float(n)
+
+# after converting, print its data type to the screen
+print(type(n))
