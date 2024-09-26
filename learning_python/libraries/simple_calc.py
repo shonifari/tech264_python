75af357665965472f30754ff7a6fefe9238205b1
Karis Omotosho <karis.omotosho@gmail.com>
2024-09-26 15:56:57 +0100

    Understanding module, packages and linraries

---
+++ learning_python/libraries/simple_calc.py
@@ -0,0 +1,9 @@
+# Mini-calculator
+
+from math_operations import add
+
+first_num = int(input("Enter the first number: "))
+second_num = int(input("Enter the first number: "))
+
+result = add(first_num, second_num)
+print(f"{first_num} + {second_num} = {result}")
