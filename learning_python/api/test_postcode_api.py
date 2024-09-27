22683f94f98d88d028bd4110dca3c68045a5dc96
Karis Omotosho <karis.omotosho@gmail.com>
2024-09-27 15:58:10 +0100

    Added Postcodes API testing

---
+++ learning_python/api/test_postcode_api.py
@@ -0,0 +1,26 @@
+import requests
+from pprint import pprint
+
+post_codes_req = requests.get("https://api.postcodes.io/postcodes/sw162xn")
+
+#
+# print(f"Status code: {post_codes_req.status_code}")
+# print(f"Headers: {post_codes_req.headers}")
+# print(f"Content: {post_codes_req.content}")
+#
+# pprint(f"Data type of JSON: {type(post_codes_req.json())}")
+# pprint(post_codes_req.json())
+
+
+# POST REQ
+
+import requests
+import json
+
+json_body = json.dumps({'postcodes': ["PR3 0SG", "M45 6GN", "EX165BL"]})
+
+headers = {'Content-Type': 'application/json'}
+
+post_multi_req = requests.post("https://api.postcodes.io/postcodes", headers=headers, data=json_body)
+
+pprint(post_multi_req.json())
